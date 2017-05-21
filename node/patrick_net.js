421fd7ac9fee6c658349bbc26edb1c1a7b1fd419
Patrick Killelea <p@patrick.net>
2017-05-21 06:44:52 +0000

    explicitly add in license

--- node/patrick_net.js
+++ node/patrick_net.js
@@ -1,3 +1,19 @@
+// ISC License
+// 
+// Copyright (c) 2017 by Patrick Killelea
+// 
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+// 
+// the software is provided "as is" and the author disclaims all warranties with
+// regard to this software including all implied warranties of merchantability
+// and fitness. In no event shall the author be liable for any special, direct,
+// indirect, or consequential damages or any damages whatsoever resulting from
+// loss of use, data or profits, whether in an action of contract, negligence
+// or other tortious action, arising out of or in connection with the use or
+// performance of this software.
+
 try { conf = require('./conf.json') } catch(e) { console.log(e.message); process.exit(1) } // conf.json is required
 
 cluster     = require('cluster')
