528dda06636369fef90c5511f1b9c2641c65e99c
Patrick Killelea <p@patrick.net>
2017-09-09 19:11:10 +0000

    put back bold, italic, and blockquote shortcuts

--- node/patrick_net.js
+++ node/patrick_net.js
@@ -302,6 +302,9 @@ Number.prototype.number_format = function() {
 String.prototype.linkify = function(ref) {
 
     let hashtagPattern   = /^#(\w+)/gim
+    let boldPattern      = /\*(.+)\*/gim
+    let italicPattern    = /_(.+)_/gim
+    let blockquotePattern= /""(.+)""/gim
     let imagePattern     = /((https?:\/\/[\w$%&~\/.\-;:=,?@\[\]+]*?)\.(jpg|jpeg|gif|gifv|png|bmp))(\s|$)/gim
     let urlPattern       = /\b(https?:\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|])(\s|$)/gim // http://, https://
     let pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))(\s|$)/gim                                    // www. sans http:// or https://
@@ -321,6 +324,9 @@ String.prototype.linkify = function(ref) {
         .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a> ')
         .replace(emailpostPattern, '<a href="mailto:$1">$1</a> ')
         .replace(linebreakPattern, '<br>')
+        .replace(boldPattern,      '<b>$1</b>')
+        .replace(italicPattern,    '<i>$1</i>')
+        .replace(blockquotePattern,'<blockquote>$1</blockquote>')
         .replace(/\0/g,            '') // do not allow null in strings
 
     result = block_unknown_iframes(result)
