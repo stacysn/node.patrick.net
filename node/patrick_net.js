9e86f4853943f50036c768c49665fcd9eda33ae2
patrick <p@patrick.net>
2018-01-14 11:41:11 -0800

    check for existence of user before getting user_name

--- node/patrick_net.js
+++ node/patrick_net.js
@@ -1308,13 +1308,15 @@ async function get_unrequited(context, d, ob, offset) {
 
 async function get_followersof(context, d, ob, offset) {
     const followersof = intval(_GET(context.req.url, 'followersof'))
-    const message = 'Followers of ' + (await get_userrow(followersof, context.db)).user_name
+    const user = await get_userrow(followersof, context.db)
+    if (!user) return ['User Not Found', []]
+
+    const message = 'Followers of ' + user.user_name
     const users = await query(`select sql_calc_found_rows * from users
         where user_id in (select rel_self_id from relationships where rel_other_id=? and rel_i_follow > 0)
         order by ${ob} ${d} limit 40 offset ${offset}`, [followersof, ob, d], context.db)
 
-    // keep followers-count cache in users table correct
-    await query('update users set user_followers=? where user_id=?', [users.length, followersof], context.db)
+    await query('update users set user_followers=? where user_id=?', [users.length, followersof], context.db) // keep followers-count cache in users table correct
 
     return [message, users]
 }
